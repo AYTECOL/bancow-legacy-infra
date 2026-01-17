a6ce88a0d9ac06cdc4afb314df1a8bc3a4230828
Andrés Arango <andres@ayte.co>
2026-01-17 17:07:52 -0500

    feat (cdk) Recommended adjustments were successfully applied to the vpc-stack.ts file. Subnets use CDK automatic discovery, Security Group is immutable, and SSM parameters are regrouped by responsibility.

--- cdk/lib/network/vpc-stack.ts
+++ cdk/lib/network/vpc-stack.ts
@@ -38,48 +38,39 @@ export class VpcStack extends cdk.NestedStack {
       },
     });
 
-    // Import private subnets by tags
-    this.privateSubnets = [
-      ec2.Subnet.fromSubnetAttributes(this, 'PrivateSubnet1', {
-        subnetId: cdk.Fn.importValue(`${projectName}-${environment}-private-subnet-1-id`),
-        availabilityZone: cdk.Fn.importValue(`${projectName}-${environment}-private-subnet-1-az`),
-      }),
-      ec2.Subnet.fromSubnetAttributes(this, 'PrivateSubnet2', {
-        subnetId: cdk.Fn.importValue(`${projectName}-${environment}-private-subnet-2-id`),
-        availabilityZone: cdk.Fn.importValue(`${projectName}-${environment}-private-subnet-2-az`),
-      }),
-    ];
-
-    // Import public subnets by tags
-    this.publicSubnets = [
-      ec2.Subnet.fromSubnetAttributes(this, 'PublicSubnet1', {
-        subnetId: cdk.Fn.importValue(`${projectName}-${environment}-public-subnet-1-id`),
-        availabilityZone: cdk.Fn.importValue(`${projectName}-${environment}-public-subnet-1-az`),
-      }),
-      ec2.Subnet.fromSubnetAttributes(this, 'PublicSubnet2', {
-        subnetId: cdk.Fn.importValue(`${projectName}-${environment}-public-subnet-2-id`),
-        availabilityZone: cdk.Fn.importValue(`${projectName}-${environment}-public-subnet-2-az`),
-      }),
-    ];
+    // Let CDK discover subnets automatically from the VPC
+    // This approach reduces coupling and is more resilient to infrastructure changes
+    this.privateSubnets = this.vpc.privateSubnets;
+    this.publicSubnets = this.vpc.publicSubnets;
 
     // Import Lambda security group
+    // Mark as immutable to prevent accidental changes from CDK
     this.lambdaSecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(
       this,
       'LambdaSecurityGroup',
       cdk.Fn.importValue(`${projectName}-${environment}-lambda-sg-id`),
+      { mutable: false }
     );
 
     // Publish VPC ID to SSM Parameter Store for SAM consumption
     new ssm.StringParameter(this, 'VpcIdParameter', {
-      parameterName: `/${projectName}/${environment}/network/vpc-id`,
+      parameterName: `/${projectName}/${environment}/network/vpc/id`,
       stringValue: this.vpc.vpcId,
       description: `VPC ID for ${projectName} ${environment}`,
       tier: ssm.ParameterTier.STANDARD,
     });
 
+    // Publish VPC CIDR for reference
+    new ssm.StringParameter(this, 'VpcCidrParameter', {
+      parameterName: `/${projectName}/${environment}/network/vpc/cidr`,
+      stringValue: this.vpc.vpcCidrBlock,
+      description: `VPC CIDR block for ${projectName} ${environment}`,
+      tier: ssm.ParameterTier.STANDARD,
+    });
+
     // Publish private subnet IDs
     new ssm.StringParameter(this, 'PrivateSubnetIdsParameter', {
-      parameterName: `/${projectName}/${environment}/network/private-subnet-ids`,
+      parameterName: `/${projectName}/${environment}/network/subnets/private`,
       stringValue: this.privateSubnets.map(s => s.subnetId).join(','),
       description: `Private subnet IDs for ${projectName} ${environment}`,
       tier: ssm.ParameterTier.STANDARD,
@@ -87,7 +78,7 @@ export class VpcStack extends cdk.NestedStack {
 
     // Publish public subnet IDs
     new ssm.StringParameter(this, 'PublicSubnetIdsParameter', {
-      parameterName: `/${projectName}/${environment}/network/public-subnet-ids`,
+      parameterName: `/${projectName}/${environment}/network/subnets/public`,
       stringValue: this.publicSubnets.map(s => s.subnetId).join(','),
       description: `Public subnet IDs for ${projectName} ${environment}`,
       tier: ssm.ParameterTier.STANDARD,
@@ -95,17 +86,9 @@ export class VpcStack extends cdk.NestedStack {
 
     // Publish Lambda security group ID
     new ssm.StringParameter(this, 'LambdaSecurityGroupParameter', {
-      parameterName: `/${projectName}/${environment}/network/lambda-sg-id`,
+      parameterName: `/${projectName}/${environment}/network/sg/lambda`,
       stringValue: this.lambdaSecurityGroup.securityGroupId,
       description: `Lambda security group ID for ${projectName} ${environment}`,
-      tier: ssm.ParameterTier.STANDARD,
-    });
-
-    // Publish VPC CIDR for reference
-    new ssm.StringParameter(this, 'VpcCidrParameter', {
-      parameterName: `/${projectName}/${environment}/network/vpc-cidr`,
-      stringValue: this.vpc.vpcCidrBlock,
-      description: `VPC CIDR block for ${projectName} ${environment}`,
       tier: ssm.ParameterTier.STANDARD,
     });
 
